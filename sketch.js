471e9741bd52316ee7f03227ba866294c055d4bf
dpbpd <bergstrom.david5@gmail.com>
2019-03-28 20:05:29 -0500

    added p5 and some color

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+    //createCanvas(windowWidth,windowHeight);
+}
+  
+function draw() {
+    createCanvas(windowWidth,windowHeight);
+    background('#76b0b6');     //('#7cb4ba'); ('#76b0b6');
+    ellipse(windowWidth / 2, windowHeight / 2, windowWidth / 3, windowHeight / 3);
+}
